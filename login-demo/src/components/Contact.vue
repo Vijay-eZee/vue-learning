eaa335074a48db0d9310997e4d10e4392b36f9a4
vijay-ezee <vijay.reshamwala@ezeetechnosys.com>
2022-05-05 18:32:33 +0530

    add teleport

--- login-demo/src/components/Contact.vue
+++ login-demo/src/components/Contact.vue
@@ -1,8 +1,31 @@
 <template>
-    contact
+  <div>
+    <button @click="isModalOpen = true">Open Modal</button>
+    <Teleport to="body">
+      <div v-if="isModalOpen" class="modal bg-green-400">
+        <p>Hello from the modal!</p>
+        <button @click="isModalOpen = false">Close</button>
+      </div>
+    </Teleport>
+  </div>
 </template>
 <script>
 export default {
-    name:'Contact'
-}
+  name: "Contact",
+  data() {
+    return {
+      isModalOpen: false,
+    };
+  },
+};
 </script>
+<style scoped>
+.modal {
+  position: fixed;
+  z-index: 999;
+  top: 20%;
+  left: 50%;
+  width: 300px;
+  margin-left: -150px;
+}
+</style>
